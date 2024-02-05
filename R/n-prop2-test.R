1c5c021ea528e5941ec2fcafb1f454897bb50945
Joe Thorley <joe@poissonconsulting.ca>
2024-02-04 17:06:58 -0800

    - renamed power_prop_test to n_prop2_test and added p_prop2_test

---
+++ R/n-prop2-test.R
@@ -0,0 +1,41 @@
+#' Sample Size for Two Sample Test Proportion
+#' 
+#' Uses `stats::power.prop.test()` to calculate the number of samples that
+#' must be included in a study 
+#' to achieve a statistical power of 0.8 at a significance level of 0.05 
+#' for a test of a difference in the proportion of samples
+#' appearing in one of two groups.
+#'
+#' @param p1 Proportion appearing in one group.
+#' @param p2 Proportion appearing in other group.
+#' @param one_sided A flag specifying whether to perform a one sided test.
+#'
+#' @return A integer of the required number of individuals. 
+#' @export
+#'
+#' @examples
+#' n_prop2_test(0.9, 0.1)
+#' n_prop2_test(0.1, 0.9)
+#' n_prop2_test(0.01, 0.09)
+#' n_prop2_test(0.45, 0.55)
+#' n_prop2_test(0.005, 0.0075)
+n_prop2_test <- function(p1, p2, one_sided = FALSE) {
+  chk_numeric(p1)
+  chk_numeric(p2)
+  chk_scalar(p1)
+  chk_scalar(p2)
+  chk_range(p1)
+  chk_range(p2)
+  chk_lte(sum(c(p1, p2)), value = 1, x_name = "The sum of `p1` and `p2`")
+  chk_flag(one_sided)
+  
+  if(is.na(p1) | is.na(p2)) {
+    return(NA_integer_)
+  }
+  
+  alternative <- if(one_sided) "one.sided" else "two.sided"
+  
+  stats::power.prop.test(p1 = p1, p2 = p2, power = 0.8, alternative = alternative)$n |>
+    ceiling() |>
+    as.integer()
+}
---
+++ R/p-prop2-test.R
@@ -0,0 +1,40 @@
+#' P-value for Exact Two Sample Test Proportions
+#' 
+#' Uses `stats::binom.test()` to calculate the exact p-value for a
+#' a two sample test of a difference in the probability of being in n1 vs n2.
+#'
+#' @param n1 Number of samples in first group.
+#' @param n2 Number of samples in second group.
+#' @param one_sided A flag specifying whether to perform a one sided test.
+#'
+#' @return A numeric scalar of the p-value. 
+#' @export
+#'
+#' @examples
+#' p_prop2_test(6, 0)
+#' p_prop2_test(3, 3)
+#' p_prop2_test(0, 6)
+#' p_prop2_test(106, 100)
+#' p_prop2_test(106, 100, one_sided = TRUE)
+#' p_prop2_test(100, 106, one_sided = TRUE)
+p_prop2_test <- function(n1, n2, one_sided = FALSE) {
+  chk_whole_number(n1)
+  chk_whole_number(n2)
+  chk_scalar(n1)
+  chk_scalar(n2)
+  chk_gte(n1)
+  chk_gte(n2)
+  chk_flag(one_sided)
+  
+  x <- n1
+  n <- n1 + n2
+  
+  alternative <- if(one_sided) {
+    if(n1 <= n2) "less" else "greater"
+  }
+  else {
+    "two.sided"
+  }
+  
+  stats::binom.test(x, n, alternative = alternative)$p.value
+}
