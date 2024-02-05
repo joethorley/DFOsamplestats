4d35367746656988fb4a430a9ddba729cd15a41c
Joe Thorley <joe@poissonconsulting.ca>
2024-02-05 07:46:06 -0800

    prop2_samplesize one_sided

--- R/prop2-samplesize.R
+++ R/prop2-samplesize.R
@@ -7,7 +7,6 @@
 #' appearing in one of two groups.
 #'
 #' @inheritParams params
-#' @param one_sided A flag specifying whether to perform a one sided test.
 #'
 #' @return A integer of the required number of individuals. 
 #' @export
@@ -18,16 +17,23 @@
 #' prop2_samplesize(0.01, 0.09)
 #' prop2_samplesize(0.45)
 #' prop2_samplesize(0.005, 0.0075)
-#' prop2_samplesize(0.005, 0.0075, one_sided = TRUE)
-prop2_samplesize <- function(p1, p2 = 1 - p1, one_sided = FALSE) {
+#' prop2_samplesize(0.005, 0.0075, alternative = "less")
+#' prop2_samplesize(0.005, 0.0075, alternative = "greater")
+prop2_samplesize <- function(p1, p2 = 1 - p1, alternative = "two.sided") {
   chk_number(p1)
   chk_number(p2)
   chk_range(p1)
   chk_range(p2)
   chk_lte(sum(c(p1, p2)), value = 1, x_name = "The sum of `p1` and `p2`")
-  chk_flag(one_sided)
+  chk_string(alternative)
+  chk_subset(alternative, c("two.sided", "greater", "less"))
   
-  alternative <- if(one_sided) "one.sided" else "two.sided"
+  if((alternative == "greater" & p1 < p2) | (alternative == "less" & p1 > p2)) {
+    return(Inf)
+  }
+  if(alternative %in% c("greater", "less")) {
+    alternative <- "one.sided"
+  }
   
   n <- stats::power.prop.test(p1 = p1, p2 = p2, power = 0.8, alternative = alternative)$n
   n <- n * 2
