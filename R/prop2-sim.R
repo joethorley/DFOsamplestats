70fe61c91cdde53081bcfc52b032bf3be92c5f2b
Joe Thorley <joe@poissonconsulting.ca>
2024-02-21 19:55:35 -0800

    prop2_sim() not accepts p2

--- R/prop2-sim.R
+++ R/prop2-sim.R
@@ -15,13 +15,19 @@
 #' prop2_sim(0.1, 10)
 #' set.seed(99)
 #' prop2_sim(0.45, 10)
-prop2_sim <- function(p1, n) {
+prop2_sim <- function(p1, n, p2 = 1 - p1) {
   chk_number(p1)
+  chk_number(p2)
   chk_range(p1)
+  chk_range(p2)
+  chk_lte(sum(c(p1, p2)), value = 1, x_name = "The sum of `p1` and `p2`")
   chk_whole_number(n)
   chk_gt(n)
   
-  n1 <- stats::rbinom(1, n, p1)
-  n2 <- n - n1
-  as.integer(c(n1, n2))
+  p <- c(p1, p2)
+  p3 <- 1 - sum(p)
+  p[3] <- p3
+  
+  samples <- stats::rmultinom(1, n, prob = p) |> as.vector()
+  samples[1:2]
 }
