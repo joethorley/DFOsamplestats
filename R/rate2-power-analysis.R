acff88792c03c809b74d57ecf0a43ad46583a689
Joe Thorley <joe@poissonconsulting.ca>
2024-02-23 06:24:16 -0800

    rate2_power_analysis

--- R/rate2-power-analysis.R
+++ R/rate2-power-analysis.R
@@ -1,5 +1,7 @@
-rate_sim_iter <- function(i, p, n) {
-  rate_sim(p, n)
+rate_sim_iter <- function(i, p, n, alternative) {
+  sim <- rate_sim(p, n)
+  effect <- rate_effect(r = sim$r, n = sim$n, alternative = alternative)
+  effect$pvalue[nrow(effect)]
 }
 
 #' Power Analysis for Rates
@@ -10,15 +12,17 @@ rate_sim_iter <- function(i, p, n) {
 #' @inheritParams params
 #' @return A real scalar of the power.
 #' @export
-rate2_power_analysis <- function(p1, n, p2 = NULL, nsims = 1000) {
+#' @examples
+#' rate2_power_analysis(0.9, 10, nsims = 100)
+#' rate2_power(0.005, p2 = 0.0075, 1000, alternative = "greater")
+rate2_power_analysis <- function(p1, n, p2 = NULL, alternative = "two.sided", nsims = 1000) {
   chk_whole_number(nsims)
   chk_gte(nsims, 10)
   
   p <- c(p1, p2)
   
-  sims <- lapply(1:nsims, FUN = rate_sim_iter, p = p, n = n)
-  # 
-  # pvalues <- lapply(sims, FUN = rate2_pvalue_fun, alternative = alternative)
-  # pvalues <- unlist(pvalues)
-  # sum(pvalues < 0.05) / length(pvalues)
+  pvalues <- vapply(1:nsims, FUN = rate_sim_iter, FUN.VALUE = 1, 
+                    p = p, n = n, alternative = alternative)
+  print(pvalues)
+  sum(pvalues < 0.05) / length(pvalues)
 }
